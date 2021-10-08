62b161d18fad4b6295b43930f3c043d8f775bcba
Cole Garien <colegarien@gmail.com>
2021-10-07 20:34:18 -0500

    leverage tail call recursion in game state and fix gitignore for vscode

--- lib/state/game.ex
+++ lib/state/game.ex
@@ -2,7 +2,12 @@ defmodule Zung.State.Game do
   @behaviour Zung.State.State
 
   @impl Zung.State.State
-  def run(%Zung.Client{} = client, data) do
+  def run(%Zung.Client{} = client, _data) do
+    # TODO good place for intro MOTD or brief of past happenings while away
+    game_loop(client)
+  end
+
+  def game_loop(client) do
     Zung.Client.write_data(client, "||NL||||RESET||> ")
     msg =
       with data <- Zung.Client.read_line(client),
@@ -13,6 +18,7 @@ defmodule Zung.State.Game do
       {:ok, output} -> Zung.Client.write_line(client, output)
       {:error, :unknown_command} -> Zung.Client.write_line(client, "||GRN||Wut?||RESET||")
     end
-    {__MODULE__, data}
+
+    game_loop(client)
   end
 end
